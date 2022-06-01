9c344f0a195535780be29b5b88a4e94f20b8e017
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-06-01 20:57:38 -0300

    ajustes no componete comics

--- src/components/Comics/Comics.jsx
+++ src/components/Comics/Comics.jsx
@@ -9,9 +9,9 @@ const Comics = () => {
 
   useEffect(() => {
     api
-      .get('/comics')
+      .get('comics')
       .then(response => { setComics(response.data.data.results) })
-      .catch(err => console.log(err))
+      .catch(err => console.log('Log erro', err))
   }, [])
 
   const handleMore = useCallback(async () => {
@@ -30,19 +30,22 @@ const Comics = () => {
     }
   }, [comics])
 
+
   return (
     <div>
       <h1>Comics</h1>
       <div className='container-items'>
-        {comics.map(comics =>
-          <div className='item img__wrap' key={comics.id}>
-            <img className='item-image' src={`${comics.thumbnail.path}/portrait_uncanny.${comics.thumbnail.extension}`} alt={comics.title} />
-            <div className='img__description'>
-              <p>{comics.title}</p>
-              <p className='featured--description'>{comics.description}</p>
-              <button>QUERO ESSE</button>
-            </div>
-          </div>)}
+        {comics.map(comic => {
+          return (
+            <div className='item img__wrap' key={comic.id}>
+              <img className='item-image' src={`${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`} alt={comic.title} />
+              <div className='img__description'>
+                <p>{comic.title}</p>
+                <p className='featured--description'>{comic.description}</p>
+                <button>QUERO ESSE</button>
+              </div>
+            </div>)
+        })}
       </div>
       <button onClick={handleMore}>Load more</button>
     </div>
