11f6fe05b0c6c63d3876e558353bda18585526e1
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-06-06 15:57:45 -0300

    ajustes configuração env

--- src/context/MapContext.jsx
+++ src/context/MapContext.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState, createContext } from 'react'
+import {useEffect, useState, createContext } from 'react'
 import { useLoadScript } from '@react-google-maps/api'
 import ReverseGeocode from '../services/ReverseGeocode';
 
@@ -9,31 +9,34 @@ export const MapProvider = ({children}) => {
     const [location, setLocation] = useState(false)
     const [Lat, setLat] = useState(null);
     const [Lng, setLng] = useState(null);
+
     const [geocodes, setGeocodes] = useState([])
 
+    const apikey = process.env.REACT_APP_API_MAPS_KEY
 
-    useEffect(() => {
+    useEffect(() => {   
         navigator.geolocation.getCurrentPosition((position) => {
             setLat(position.coords.latitude);
             setLng(position.coords.longitude);
             setLocation(true)
         })
+        
+        ReverseGeocode
+        .get(`json?latlng=${Lat},${Lng}&key=${apikey}`)
+        .then(response => { setGeocodes(response.results) })
+        .catch(err => console.log('Log erro', err))
     }, [])
-
-    const textEncoded = `json?latlng=${Lat}, ${Lng}&key=AIzaSyAwYlFA-Qhq7YdG1Yyk2LHC0KOfwXT3n_0`
-
-    ReverseGeocode
-    .get(encodeURIComponent(textEncoded))
-    .then(response => { setGeocodes(response.results) })
-    .catch(err => console.log('Log erro', err))
 
     const centro = {
         lat: Lat,
         lng: Lng
     }
 
+console.log(Lat)
+
+
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.COMIC_APP_API_MAPS_KEY
+        googleMapsApiKey: apikey
     })
 //codes
 
--- src/services/ReverseGeocode.js
+++ src/services/ReverseGeocode.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const ReverseGeocode = axios.create({
     baseURL: 'https://maps.googleapis.com/maps/api/geocode/',
+    headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 })
 
 export default ReverseGeocode
